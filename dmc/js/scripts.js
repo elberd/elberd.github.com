3c7d3160ce82f6a815f9ef21a3ef050282d448f4
elberd <berd@ant.ms>
2016-03-09 15:19:27 +0300

    Added files via upload
    erere

---
+++ dmc/js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	$('.search').on('click', function(e){
+		
+		if ($(this).hasClass('act-search'))
+		{
+			$('#search-form').submit();
+			$(this).removeClass('act-search');
+		}
+		else
+		{
+			$('#search-form').css('display','block')
+			$('#search-form input').focus()
+			$('.menu ul').css('visibility','hidden')
+			$(this).addClass('act-search')
+		} 
+		e.stopPropagation?e.stopPropagation():e.cancelBubble = true
+	})
+
+	$('#search-form').on('click', function(e){
+		e.stopPropagation?e.stopPropagation():e.cancelBubble = true
+	})
+
+	$('body').on('click', function(){
+		$('#search-form').css('display','none')
+		$('.search').removeClass('act-search')
+		$('.menu ul').css('visibility','visible')
+	})
+
+})
